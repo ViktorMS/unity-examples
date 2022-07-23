5734d8de1b10ab402772cf3d5d1127d19e55d528
Viktor.ms <vjh2@hi.is>
2022-07-23 15:03:17 +0000

    Add files via upload

---
+++ StarterAssetsInputs.cs
@@ -0,0 +1,98 @@
+using Unity.Netcode;
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+	public class StarterAssetsInputs : NetworkBehaviour
+	{
+		[Header("Character Input Values")]
+		public Vector2 move;
+		public Vector2 look;
+		public bool jump;
+		public bool sprint;
+
+		[Header("Movement Settings")]
+		public bool analogMovement;
+
+		[Header("Mouse Cursor Settings")]
+		public bool cursorLocked = true;
+		public bool cursorInputForLook = true;
+
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+
+        private void Start()
+        {
+			if (!IsOwner) return;
+			enabled = true;
+		}
+
+        public void OnMove(InputValue value)
+		{
+			if (!enabled) return;
+			MoveInput(value.Get<Vector2>());
+		}
+
+		public void OnLook(InputValue value)
+		{
+			if (!enabled) return;
+			if (cursorInputForLook)
+			{
+				LookInput(value.Get<Vector2>());
+			}
+		}
+
+		public void OnJump(InputValue value)
+		{
+			if (!enabled) return;
+			JumpInput(value.isPressed);
+		}
+
+		public void OnSprint(InputValue value)
+		{
+			if (!enabled) return;
+			SprintInput(value.isPressed);
+		}
+#endif
+
+
+		public void MoveInput(Vector2 newMoveDirection)
+		{
+			if (!IsOwner) return;
+			move = newMoveDirection;
+		} 
+
+		public void LookInput(Vector2 newLookDirection)
+		{
+			if (!IsOwner) return;
+			look = newLookDirection;
+		}
+
+		public void JumpInput(bool newJumpState)
+		{
+			if (!IsOwner) return;
+			jump = newJumpState;
+		}
+
+		public void SprintInput(bool newSprintState)
+		{
+			if (!IsOwner) return;
+			sprint = newSprintState;
+		}
+
+		private void OnApplicationFocus(bool hasFocus)
+		{
+			if (!IsOwner) return;
+			SetCursorState(cursorLocked);
+		}
+
+		private void SetCursorState(bool newState)
+		{
+			if (!IsOwner) return;
+			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+		}
+	}
+	
+}
